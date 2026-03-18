e6dc7e946a7d9c9981f8df25b75c7c91d3f6a0d7
Banda Ajitesh <ajitesh0314@gmail.com>
2026-03-18 09:01:31 +0530

    Update momentum_strategy.py

--- momentum_strategy.py
+++ momentum_strategy.py
@@ -1,8 +1,10 @@
+# IMPORTS
 import numpy as np
 import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
 
+# DATA
 stocks = pd.read_csv("constituents.csv")
 
 tickers = stocks["Symbol"].tolist()
@@ -20,33 +22,38 @@ prices = prices.ffill()
 prices = prices.dropna()
 print(len(prices))
 
+# RETURNS
 returns = prices.pct_change().dropna()
 
+# MOMENTUM
 window= 126
 momentum = prices.shift(21).pct_change(126)
 print(momentum.head())
 print(momentum.tail())
 
+# RANKING
 ranks = momentum.rank(axis=1, ascending=False)
 percentile = momentum.rank(axis=1, pct=True)
 
 print(ranks.head())
 print(percentile.head())
 
+# SIGNALS
 winners = percentile >= 0.99
 losers = percentile <= 0.01
 
-
+# POSITIONS
 positions = pd.DataFrame(0, index=momentum.index, columns=momentum.columns)
 positions[winners] = 1
 positions[losers] = -1
 positions = positions.div(positions.abs().sum(axis=1), axis=0)
 positions = positions.shift(1)
 
+# STRATEGY RETURNS
 strategy_returns = (positions * returns).sum(axis=1)
 
+# PERFORMANCE
 equity_curve = (1 + strategy_returns).cumprod()
-
 
 sharpe = np.sqrt(252) * strategy_returns.mean() / strategy_returns.std()
 print("Sharpe Ratio:", sharpe)
@@ -57,6 +64,7 @@ max_dd = drawdown.min()
 
 print("Max Drawdown:", max_dd)
 
+# PLOT
 equity_curve.plot(figsize=(10,5))
 plt.title("Momentum Strategy Equity Curve")
 plt.xlabel("Date")
